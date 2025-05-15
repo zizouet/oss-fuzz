c790a51d4cb1859de7300088a8fcf778bc4d2f1b
zizouet <francois.theron@epfl.ch>
2025-05-15 15:52:03 +0200

    BETTER

--- SUBMISSION/part1/run.w_corpus.sh
+++ SUBMISSION/part1/run.w_corpus.sh
@@ -10,13 +10,14 @@ trap cleanup EXIT
 
 PROJECT_NAME="freetype2"
 FUZZER_NAME="ftfuzzer"
+CORPUS_NAME="build/out/run.w_corpus/"
 RUN_TIME=$((4 * 60 * 60))
 
 
 FUZZER_ARGS="-rss_limit_mb=2560 -fork=$(nproc) -ignore_crashes=1"
 
 echo "Building output folder"
-mkdir build/out/run.w_corpus
+mkdir "$CORPUS_NAME"
 
 echo "Building image for $PROJECT_NAME"
 python3 infra/helper.py build_image "$PROJECT_NAME"
@@ -25,11 +26,11 @@ echo "Building fuzzers for $PROJECT_NAME"
 python3 infra/helper.py build_fuzzers "$PROJECT_NAME"
 
 echo "Running $FUZZER_NAME for 4 hours"
-python3 infra/helper.py run_fuzzer "$PROJECT_NAME" "$FUZZER_NAME" --corpus-dir build/out/run.w_corpus --run_time="$RUN_TIME" -e FUZZER_ARGS="$FUZZER_ARGS"
+python3 infra/helper.py run_fuzzer "$PROJECT_NAME" "$FUZZER_NAME" --corpus-dir "$CORPUS_NAME" --run_time="$RUN_TIME" -e FUZZER_ARGS="$FUZZER_ARGS"
 
 echo "Creating coverage report"
 python3 infra/helper.py build_fuzzers --sanitizer coverage freetype2
-python3 infra/helper.py coverage freetype2 --corpus-dir build/out/run.w_corpus/ --fuzz-target ftfuzzer
+python3 infra/helper.py coverage freetype2 --corpus-dir "$CORPUS_NAME" --fuzz-target ftfuzzer
 
 
 
