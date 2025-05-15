370266691dd2f32158a091d95f1ff76539e1d7b2
zizouet <francois.theron@epfl.ch>
2025-05-15 15:41:56 +0200

    fix

--- SUBMISSION/part1/run.w_o_corpus.sh
+++ SUBMISSION/part1/run.w_o_corpus.sh
@@ -11,6 +11,7 @@ trap cleanup EXIT
 PROJECT_NAME="freetype2"
 FUZZER_NAME="ftfuzzer"
 RUN_TIME=$((4 * 60 * 60))
+CORPUS_NAME="build/out/run.w__ocorpus/"
 FUZZER_ARGS="-rss_limit_mb=2560 -fork=$(nproc) -ignore_crashes=1"
 
 echo "Applying git diff"
@@ -18,7 +19,7 @@ git apply SUBMISSION/part1/oss-fuzz.diff || { echo "Patch failed"; exit 1; }
 git apply SUBMISSION/part1/project.diff || { echo "Patch failed"; exit 1; }
 
 echo "Building output folder"
-mkdir build/out/run.w_o_corpus
+mkdir "$CORPUS_NAME"
 
 echo "Building image for $PROJECT_NAME"
 python3 infra/helper.py build_image "$PROJECT_NAME"
@@ -27,11 +28,11 @@ echo "Building fuzzers for $PROJECT_NAME"
 python3 infra/helper.py build_fuzzers "$PROJECT_NAME"
 
 echo "Running $FUZZER_NAME for 4 hours"
-python3 infra/helper.py run_fuzzer "$PROJECT_NAME" "$FUZZER_NAME" --corpus-dir build/out/run.w_o_corpus --run_time="$RUN_TIME" -e FUZZER_ARGS="$FUZZER_ARGS"
+python3 infra/helper.py run_fuzzer "$PROJECT_NAME" "$FUZZER_NAME" --corpus-dir "$CORPUS_NAME" --run_time="$RUN_TIME" -e FUZZER_ARGS="$FUZZER_ARGS"
 
 echo "Creating coverage report"
-python3 infra/helper.py build_fuzzers --sanitizer coverage freetype2
-python3 infra/helper.py coverage freetype2 --corpus-dir build/out/run.w__ocorpus/ --fuzz-target ftfuzzer
+python3 infra/helper.py build_fuzzers --sanitizer coverage "$PROJECT_NAME"
+python3 infra/helper.py coverage "$PROJECT_NAME" --corpus-dir "$CORPUS_NAME" --fuzz-target ftfuzzer
 
 
 
