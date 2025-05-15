ff16e3de917315d8e71d20b4a8d563305e82f157
zizouet <francois.theron@epfl.ch>
2025-05-15 15:26:41 +0200

    feat:baseline

--- SUBMISSION/part3/improve1/run.improve1.sh
+++ SUBMISSION/part3/improve1/run.improve1.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status
+# Define cleanup function
+cleanup() {
+  echo "Cleaning up..."
+  git reset --hard
+}
 
+# Register the cleanup function to run on EXIT signal
+trap cleanup EXIT
 cp SUBMISSION/part3/improve1/ftfuzzer.cc projects/freetype2/
 cp SUBMISSION/part3/improve1/Dockerfile projects/freetype2/
 
